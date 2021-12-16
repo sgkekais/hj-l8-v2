78f21b6bb452e763d27df53ffcb769b21b8f494a
sgkekais <kvnkaiser@gmail.com>
2021-12-16 22:48:46 +0100

    initial commit

---
+++ database/migrations/2021_12_16_213543_add_measurements_column_to_works_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMeasurementsColumnToWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('works', function (Blueprint $table) {
+            //
+            $table->string('measurements')->after('materials_used')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('works', function (Blueprint $table) {
+            //
+            $table->dropColumn('measurements');
+        });
+    }
+}
